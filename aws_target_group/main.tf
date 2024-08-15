1a639a4f57ec3ca25321c3366805ceae71b3f7b1
Francisco Wesley <98036757+FWesleycosta@users.noreply.github.com>
2024-08-14 22:05:06 -0300

    add conditional count in the module aws_target_group

--- aws_target_group/main.tf
+++ aws_target_group/main.tf
@@ -1,8 +1,10 @@
 resource "aws_lb_target_group" "this" {
-  count = length(var.target_groups)
+  count = var.create_target_group ? length(var.target_groups) : 0
 
   name_prefix = substr(var.target_groups[count.index].name_prefix, 0, 6)
-  vpc_id      = var.target_groups[count.index].vpc_id
+  
+  vpc_id = lookup(var.target_groups[count.index], "target_type", "instance") == "instance" ? var.target_groups[count.index].vpc_id : null
+  
   protocol    = var.target_groups[count.index].protocol
   port        = var.target_groups[count.index].port
   target_type = lookup(var.target_groups[count.index], "target_type", "instance")
@@ -11,7 +13,7 @@ resource "aws_lb_target_group" "this" {
     enabled  = var.target_groups[count.index].health_check_enabled
     path     = lookup(var.target_groups[count.index], "health_check_path", null)
     port     = lookup(var.target_groups[count.index], "health_check_port", null)
-    protocol = lookup(var.target_groups[count.index], "health_check_protocol", var.target_groups[count.index].protocol)
+    protocol = lookup(var.target_groups[count.index].health_check_protocol, var.target_groups[count.index].protocol)
   }
 
   deregistration_delay = lookup(var.target_groups[count.index], "deregistration_delay", 300)
