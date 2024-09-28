ba22d17c39189b159636b5f4efb3295569c911f4
Francisco Wesley <fwesleyper@gmail.com>
2024-09-28 13:18:08 -0300

    feat(cognito_user_pool_client) change version module aws_cognito_user_pool_client

--- aws_cognito_user_pool_client/versions.tf
+++ aws_cognito_user_pool_client/versions.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.7.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.54.1"
+      version = "~> 5.43.0"
     }
   }
 }
